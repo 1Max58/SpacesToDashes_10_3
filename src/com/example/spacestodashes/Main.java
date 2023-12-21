000a10a3480cca2a1e2b5c51eb35e4b7c3e1cbfc
1Max58 <mkorsun1@stu.vistula.edu.pl>
2023-12-21 11:25:07 +0200

    Copying with replacement all spaces with dashes implemented

--- src/com/example/spacestodashes/Main.java
+++ src/com/example/spacestodashes/Main.java
@@ -1,7 +1,22 @@
 package com.example.spacestodashes;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        int bajt;
+        try (
+                FileInputStream fis = new FileInputStream("test123.txt");
+                FileOutputStream fos = new FileOutputStream("test123_.txt")
+            )   {
+            do {
+                bajt = fis.read();
+                if (bajt != -1) fos.write(bajt != ' ' ? bajt : '_');
+            } while (bajt != -1);
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e);
+        }
     }
 }
